3f9573841dcfeaed18e83f3ce1a0f9559390340f
SpotAlfa <spotalfax@gmail.com>
2019-02-10 23:45:37 +0300

    Remove extra entities because it is simpler

--- src.php
+++ src.php
@@ -1,86 +1,16 @@
 <?php
 
-class CrazyException extends Exception
-{
-}
-
 trait CrazyRevision
 {
 }
 
-class Import
+class CrazyException extends Exception
 {
-    public $accessModifier;
-    public $methodName;
-    public $conditions;
-    public $callbacks;
-
-    public function __construct(string $accessModifier, string $methodName, ?string $conditions, ?string $callbacks)
-    {
-        $this->accessModifier = $accessModifier;
-        $this->methodName = $methodName;
-        $this->conditions = $conditions;
-        $this->callbacks = $callbacks;
-    }
 }
-
-function replaceRangeConditions(array $matches): string
-{
-    $subject = array_shift($matches);
-    $subject = trim($subject, '[]');
-    $bounds = explode('..', $subject);
-
-    if (ltrim($subject, '.') != $subject) {
-        $condition = sprintf('$var <= %d', array_shift($bounds));
-    } elseif (rtrim($subject, '.' != $subject)) {
-        $condition = sprintf('$var >= %d', array_shift($bounds));
-    } else {
-        list($bottom, $top) = $bounds;
-        $condition = sprintf('($var >= %d && $var <= %d)', $bottom, $top);
-    }
-
-    return $condition;
-}
-
-function replaceInjectedConditions(array $matches): string {
-    $subject = array_shift($matches);
-    $subject = trim($subject, '`');
-    $subject = sprintf('(%s)', $subject);
-
-    return $subject;
-}
-
-const DEF_PATTERN = '/^\s*(public|protected|private)\s*(static)?\s*\$[a-z][a-z\d]*(\s*,\s*\$[a-z][a-z\d]*)*.*?\s*\/\/>.+?$/i';
-const PROP_PATTERN = '/(?<=\$)[a-z][a-z\d]*/i';
-const COMMENT_PATTERN = '/(?<=\/\/>).+?$/';
-
-const TYPE_DEF_PATTERN = '/^([a-z][a-z\d]*(<(?1)>)?(\[\])*)/i';
-const ACC_MOD_PATTERN = '/^(\+|#|-)/';
-const IMPORT_PATTERN = '/^[a-z][a-z\d]*/i';
-const COND_PATTERN = '/^((\()?(\[(\d+(\.\d+)?)?\.\.(?(4)(\d+(\.\d+)?)?|(\d+(\.\d+)?))\]|`([^`]|(?<=\\\\`))+`)(\s*(&&|\|\|)\s*(?1))*(?(2)\)))/';
-const CALLBACKS_PATTERN = '/(?<=^->)\s*([a-z][a-z\d]*|`([^`]|(?<=\\\\`))+`)(\s+&&\s+([a-z][a-z\d]*|`([^`]|(?<=\\\\`))+`))*/i';
-const ALIAS_PATTERN = '/(?<=^=>)\s+[a-z][a-z\d]*/';
-const USR_COMMENT_PATTERN = '/^--.+/';
-
-const RANGE_COND_PATTERN = '/\[(\d+(\.\d+)?)?\.\.(?(1)(\d+(\.\d+)?)?|(\d+(\.\d+)?))\]/';
-const INJ_CODE_PATTERN = '/`([^`]|(?<=\\\\`))+`/';
-
-const COMMENT_START = '//>';
-const CALLBACKS_SIGN = '->';
-const ALIAS_SIGN = '=>';
-const CONJUNCTION = '&&';
-
-const GETTER_TEMPLATE = 'function (): %s { $var = %s; %s; }';
-const SETTER_TEMPLATE = 'function (%s $var): void { %s; }';
-const GENERAL_TEMPLATE = 'function (...$args) { %s; }';
-
-const COND_CLAUSE = 'if (%s) { %s; %s; } else { throw new CrazyException("Conditions for %s::%s() did not pass"); }';
-const NON_COND_CLAUSE = '{ %s; %s; }';
-
-const PREDEFINED_CLASSES = 135;
 
 $classes = get_declared_classes();
-$userDefinedClasses = array_slice($classes, PREDEFINED_CLASSES);
+$predefinedClasses = 135;
+$userDefinedClasses = array_slice($classes, $predefinedClasses);
 
 foreach ($userDefinedClasses as $className) {
     /** @noinspection PhpUnhandledExceptionInspection */
@@ -98,23 +28,28 @@ foreach ($userDefinedClasses as $className) {
 
     foreach ($file as $lineNumber => $line) {
         $isValidLineNumber = ($lineNumber >= $startLine) && ($lineNumber <= $endLine);
-        $isDefinition = (bool)preg_match(DEF_PATTERN, $line);
+        $definitionPattern = '/^\s*(public|protected|private)\s*(static)?\s*\$[a-z][a-z\d]*' .
+            '(\s*,\s*\$[a-z][a-z\d]*)*.*?\s*\/\/>.+?$/i';
+        $isDefinition = (bool)preg_match($definitionPattern, $line);
         if (!($isValidLineNumber && $isDefinition)) {
             continue;
         }
 
-        $commentDelimiter = strpos($line, COMMENT_START);
+        $commentDelimiter = strpos($line, '//>');
         $propertiesDefinition = substr($line, 0, $commentDelimiter);
         $commentDefinition = substr($line, $commentDelimiter);
 
-        preg_match_all(PROP_PATTERN, $propertiesDefinition, $matches);
+        $propertyPattern = '/(?<=\$)[a-z][a-z\d]*/i';
+        preg_match_all($propertyPattern, $propertiesDefinition, $matches);
         $properties = array_shift($matches);
 
-        preg_match(COMMENT_PATTERN, $commentDefinition, $matches);
+        $commentPattern = '/(?<=\/\/>).+?$/';
+        preg_match($commentPattern, $commentDefinition, $matches);
         $comment = array_shift($matches);
 
         $comment = trim($comment);
-        preg_match(TYPE_DEF_PATTERN, $comment, $matches);
+        $typeDeclarationPattern = '/^([a-z][a-z\d]*(<(?1)>)?(\[\])*)/i';
+        preg_match($typeDeclarationPattern, $comment, $matches);
         if (!empty($matches)) {
             $typeDeclaration = array_shift($matches);
             $comment = substr($comment, strlen($typeDeclaration));
@@ -127,13 +62,14 @@ foreach ($userDefinedClasses as $className) {
         $typeReference = $typeDeclaration;
         $primitiveTypes = ['int', 'float', 'double', 'bool', 'string', 'callable', 'object', 'resource', 'iterable'];
         if (in_array($typeDeclaration, $primitiveTypes)) {
-            $typeReference = sprintf('Crazy%s', $typeReference);
+            $typeReference = sprintf('Crazy%s', ucfirst($typeReference));
         }
 
         $imports = [];
         while (true) {
             $comment = trim($comment);
-            preg_match(ACC_MOD_PATTERN, $comment, $matches);
+            $accessModifierPattern = '/^(\+|#|-)/';
+            preg_match($accessModifierPattern, $comment, $matches);
             if (!empty($matches)) {
                 $accessModifier = array_shift($matches);
                 $comment = substr($comment, strlen($accessModifier));
@@ -142,9 +78,10 @@ foreach ($userDefinedClasses as $className) {
             }
 
             $comment = trim($comment);
-            preg_match(IMPORT_PATTERN, $comment, $matches);
-            if (!empty($matches)) {
-                $methodName = array_shift($matches);
+            $importPattern = '/^[a-z][a-z\d]*/i';
+            preg_match($importPattern, $comment, $matches);
+            $methodName = array_shift($matches);
+            if (isset($methodName)) {
                 $comment = substr($comment, strlen($methodName));
             } else {
                 /** @noinspection PhpUnhandledExceptionInspection */
@@ -152,42 +89,80 @@ foreach ($userDefinedClasses as $className) {
             }
 
             $comment = trim($comment);
-            preg_match(COND_PATTERN, $comment, $matches);
-            if (!empty($matches)) {
-                $conditions = array_shift($matches);
+            $conditionsPattern = '/^((\()?(\[(\d+(\.\d+)?)?\.\.(?(4)(\d+(\.\d+)?)?|(\d+(\.\d+)?))\]' .
+                '|`([^`]|(?<=\\\\`))+`)(\s*(&&|\|\|)\s*(?1))*(?(2)\)))/';
+            preg_match($conditionsPattern, $comment, $matches);
+            $conditions = array_shift($matches);
+            if (isset($conditions)) {
                 $comment = substr($comment, strlen($conditions));
 
-                $conditions = preg_replace_callback(RANGE_COND_PATTERN, 'replaceRangeConditions', $conditions);
-                $conditions = preg_replace_callback(INJ_CODE_PATTERN, 'replaceInjectedConditions', $conditions);
+                $rangeConditionsPattern = '/\[(\d+(\.\d+)?)?\.\.(?(1)(\d+(\.\d+)?)?|(\d+(\.\d+)?))\]/';
+                $replaceRangeConditions = function (array $matches): string {
+                    $subject = array_shift($matches);
+                    $subject = trim($subject, '[]');
+                    $bounds = explode('..', $subject);
+
+                    if (ltrim($subject, '.') != $subject) {
+                        $condition = sprintf('$var <= %d', array_shift($bounds));
+                    } elseif (rtrim($subject, '.') != $subject) {
+                        $condition = sprintf('$var >= %d', array_shift($bounds));
+                    } else {
+                        list($bottom, $top) = $bounds;
+                        $condition = sprintf('($var >= %d && $var <= %d)', $bottom, $top);
+                    }
+
+                    return $condition;
+                };
+
+                $injectedCodePattern = '/`([^`]|(?<=\\\\`))+`/';
+                $replaceInjectedConditions = function (array $matches): string {
+                    $subject = array_shift($matches);
+                    $subject = trim($subject, '`');
+                    $subject = sprintf('(%s)', $subject);
+
+                    return $subject;
+                };
+
+                $conditions = preg_replace_callback($rangeConditionsPattern, $replaceRangeConditions, $conditions);
+                $conditions = preg_replace_callback($injectedCodePattern, $replaceInjectedConditions, $conditions);
             }
 
             $comment = trim($comment);
-            preg_match(CALLBACKS_PATTERN, $comment, $matches);
-            if (!empty($matches)) {
-                $callbacks = array_shift($matches);
-                $comment = substr($comment, strlen(CALLBACKS_SIGN) + strlen($callbacks));
+            $callbacksPattern = '/(?<=^->)\s*([a-z][a-z\d]*|`([^`]|(?<=\\\\`))+`)' .
+                '(\s+&&\s+([a-z][a-z\d]*|`([^`]|(?<=\\\\`))+`))*/i';
+            preg_match($callbacksPattern, $comment, $matches);
+            $callbacks = array_shift($matches);
+            if (isset($callbacks)) {
+                $comment = substr($comment, strlen('->') + strlen($callbacks));
 
-                $callbacksElements = explode(CONJUNCTION, $callbacks);
+                $callbacksElements = explode('&&', $callbacks);
                 foreach ($callbacksElements as &$element) {
                     $element = trim($element);
                     $withoutQuotes = trim($element, '`');
                     if ($withoutQuotes != $element) {
                         $element = $withoutQuotes;
                     } else {
-                        $element = sprintf('$var = %s::%s($var)', $typeReference, $element);
+                        $element = sprintf('%s::%s($var)', $typeReference, $element);
                     }
                 }
 
                 $callbacks = implode('; ', $callbacksElements);
             }
 
-            $import = new Import($accessModifier, $methodName, @$conditions, @$callbacks);
+            $import = new stdClass();
+            $import->accessModifier = $accessModifier;
+            $import->methodName = $methodName;
+            $import->conditions = @$conditions;
+            $import->callbacks = @$callbacks;
+
             $imports[] = $import;
         }
 
         $comment = trim($comment);
-        preg_match(ALIAS_PATTERN, $comment, $matches);
-        if (!empty($matches)) {
+        $aliasPattern = '/(?<=^=>)\s+[a-z][a-z\d]*/';
+        preg_match($aliasPattern, $comment, $matches);
+        $alias = array_shift($matches);
+        if (isset($alias)) {
             if (count($properties) > 1) {
                 /** @noinspection PhpUnhandledExceptionInspection */
                 throw new CrazyException(
@@ -195,14 +170,13 @@ foreach ($userDefinedClasses as $className) {
                 );
             }
 
-            $alias = array_shift($matches);
-            $comment = substr($comment, strlen(ALIAS_SIGN) + strlen($alias));
-
+            $comment = substr($comment, strlen('=>') + strlen($alias));
             $alias = trim($alias);
         }
 
         $comment = trim($comment);
-        preg_match(USR_COMMENT_PATTERN, $comment, $matches);
+        $userCommentPattern = '/^--.+/';
+        preg_match($userCommentPattern, $comment, $matches);
         $userComment = array_shift($matches);
         $comment = substr($comment, strlen($userComment));
 
@@ -232,47 +206,42 @@ foreach ($userDefinedClasses as $className) {
                         break;
                     default:
                         $action = sprintf(
-                            'return %s::%s(%s, ...$args)', $typeReference, $import->methodName, $propertyReference
+                            'return %s::%s(%s, $var, ...$args)',$typeReference, $import->methodName, $propertyReference
                         );
                 }
-                if (isset($conditions)) {
+
+                if (isset($import->conditions)) {
+                    $conditionalTemplate = 'if (%s) { %s; %s; } ' .
+                        'else { throw new CrazyException("Conditions for %s::%s() did not pass"); }';
                     $code = sprintf(
-                        COND_CLAUSE,
-                        $import->conditions,
-                        $import->callbacks,
-                        $action,
-                        $class->name,
-                        $method
+                        $conditionalTemplate, $import->conditions, $import->callbacks, $action, $class->name,  $method
                     );
                 } else {
-                    $code = sprintf(NON_COND_CLAUSE, $import->callbacks, $action);
+                    $nonConditionalClauseTemplate = '{ %s; %s; }';
+                    $code = sprintf($nonConditionalClauseTemplate, $import->callbacks, $action);
                 }
+
                 switch ($import->methodName) {
                     case 'get':
-                        $function = sprintf(GETTER_TEMPLATE, $realType, $propertyReference, $code);
+                        $getterTemplate = 'function (): %s { $var = %s; %s; }';
+                        $function = sprintf($getterTemplate, $realType, $propertyReference, $code);
                         break;
                     case 'set':
-                        $function = sprintf(SETTER_TEMPLATE, $realType, $code);
+                        $setterTemplate = 'function (%s $var): void { %s; }';
+                        $function = sprintf($setterTemplate, $realType, $code);
                         break;
                     default:
-                        $function = sprintf(GENERAL_TEMPLATE, $code);
+                        $generalTemplate = 'function (...$args) { $var = %s; %s; }';
+                        $function = sprintf($generalTemplate, $propertyReference, $code);
                 }
 
                 $closure = eval(sprintf('return %s;', $function));
                 $flags = 0;
+
+                $accessModifiers = ['+' => ZEND_ACC_PUBLIC, '#' => ZEND_ACC_PROTECTED, '-' => ZEND_ACC_STATIC];
+                $flags |= $accessModifiers[$accessModifier];
                 if (strpos($propertiesDefinition, 'static')) {
                     $flags |= ZEND_ACC_STATIC;
-                }
-                switch ($import->accessModifier) {
-                    case '+':
-                        $flags |= ZEND_ACC_PUBLIC;
-                        break;
-                    case '#':
-                        $flags |= ZEND_ACC_PROTECTED;
-                        break;
-                    case '-':
-                        $flags |= ZEND_ACC_PRIVATE;
-                        break;
                 }
 
                 uopz_add_function($class->name, $method, $closure, $flags, true);
